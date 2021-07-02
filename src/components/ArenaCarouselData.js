97849e588b20544b4a4284d3aef3574ebd829db9
Lachlan <lachlanhtsharp@gmail.com>
2021-07-02 12:38:51 -0400

    Fixes mobile sizing

--- src/components/ArenaCarouselData.js
+++ src/components/ArenaCarouselData.js
@@ -4,16 +4,16 @@ export const CarouselData = [
     {
         title: 'Arena 1',
         image: 'https://images.adsttc.com/media/images/5ef1/68f8/b357/6529/f500/0347/large_jpg/yc8m6r-A_jpeg.jpg?1592879340',
-        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut gravida mi. Fusce eget risus vel orci pulvinar molestie. Nulla facilisi. Fusce maximus lorem ac orci tincidunt aliquam. Cras volutpat quam quis mauris aliquam, ac pretium purus tincidunt. Ut fermentum lacus a arcu rhoncus commodo.'
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut gravida mi. Fusce eget risus vel orci pulvinar molestie. Nulla facilisi. Fusce maximus lorem ac orci tincidunt aliquam. Cras volutpat quam quis mauris aliquam, ac pretium purus tincidunt.'
     },
     {
         title: 'Arena 2',
         image: 'https://static.dezeen.com/uploads/2018/04/arlington-esports-arena-populous_dezeen_2364_col_6.jpg',
-        details: 'Arena Two. Filler'
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut gravida mi. Fusce eget risus vel orci pulvinar molestie. Nulla facilisi. Fusce maximus lorem ac orci tincidunt aliquam. Cras volutpat quam quis mauris aliquam, ac pretium purus tincidunt.'
     },
     {
         title: 'Arena 3',
         image: 'https://populous.com/wp-content/uploads/2018/12/DSC08300_STAGE.jpg',
-        details: 'Arena Three. Filler'
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut gravida mi. Fusce eget risus vel orci pulvinar molestie. Nulla facilisi. Fusce maximus lorem ac orci tincidunt aliquam. Cras volutpat quam quis mauris aliquam, ac pretium purus tincidunt.'
     }
 ]
